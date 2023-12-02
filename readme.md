0a3e068877b27b60e9c6c45033e1b01b691337db
tangsheng <imtangsheng@outlook.com>
2023-12-02 18:44:58 +0800

    创建不安全的 Web 应用程序

--- readme.md
+++ readme.md
@@ -13,3 +13,5 @@ https://spring.io/projects/spring-boot
 - 参考，下载推荐的java版本，然后根据教程下载创建一个新项目hello world构建
 - 配置选择jdk版本，java的目录，同gradle配置。然后构建工具选择gradle的目录。等待自动下载构建完成项目构建
 - 运行构建选项jdk，模板，运行配置。运行。
+  信息发送失败。
+  
---
+++ src/main/java/com/robot/RobotServer/services/TestController.java
@@ -0,0 +1,12 @@
+package com.robot.RobotServer.services;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+}
